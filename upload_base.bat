b1b48c3acc18a867778b3be6b1e6f6e8a0e80359
Peter Buchmann <peter.buchmann@web.de>
2014-10-14 14:14:11 +0200

    times layout - v0.45

--- upload_base.bat
+++ upload_base.bat
@@ -1,4 +1,1 @@
-REM goapp is a wrapper around appcfg.py
-REM goapp.bat   deploy
-REM appcfg.py rollback .
-    appcfg.py --email=peter.buchmann@web.de update   mod01/mod01.yaml mod02/mod02.yaml
+appcfg.py --email=peter.buchmann@web.de update   mod01/mod01.yaml 
--- upload_extd.bat
+++ upload_extd.bat
@@ -1,7 +1,10 @@
+REM goapp is a wrapper around appcfg.py
 REM appcfg is an inclusion of goapp.bat
-REM 
+REM goapp.bat   deploy
+REM appcfg.py --email=peter.buchmann@web.de appcfg.py rollback .
 REM appcfg.py --email=peter.buchmann@web.de update_indexes .
 REM appcfg.py --email=peter.buchmann@web.de update_cron    .
 REM appcfg.py --email=peter.buchmann@web.de update         .
-    appcfg.py --email=peter.buchmann@web.de vacuum_indexes .
-    appcfg.py --email=peter.buchmann@web.de update   .
+REM appcfg.py --email=peter.buchmann@web.de vacuum_indexes .
+REM appcfg.py --email=peter.buchmann@web.de update   .
+    appcfg.py --email=peter.buchmann@web.de update   mod01/mod01.yaml mod02/mod02.yaml
