285d4fc4217ce6ec8a2353ef8c038c695543b399
Peter Buchmann <peter.buchmann@web.de>
2015-06-14 17:01:01 +0200

    package foscam, oauth, write-methods outsourced

--- mod01/main.go
+++ mod01/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"net/http"
-
-	"fmt"
-	"io"
 	"log"
 
 	_ "net/http/pprof" // profiling
@@ -14,26 +10,22 @@ import (
 	_ "github.com/pbberlin/tools/backend"
 	_ "github.com/pbberlin/tools/big_query"
 	_ "github.com/pbberlin/tools/blobstore_mgt"
-	_ "github.com/pbberlin/tools/dsu_ancestored_urls"
-	_ "github.com/pbberlin/tools/dsu_persistent_cursor"
+	_ "github.com/pbberlin/tools/dsu/ancestored_urls"
+	_ "github.com/pbberlin/tools/dsu/persistent_cursor"
 	_ "github.com/pbberlin/tools/email"
-	_ "github.com/pbberlin/tools/fetch"
+	_ "github.com/pbberlin/tools/foscam"
 	_ "github.com/pbberlin/tools/fulltext"
 	_ "github.com/pbberlin/tools/guestbook"
 	_ "github.com/pbberlin/tools/instance_mgt"
 	_ "github.com/pbberlin/tools/json"
 	_ "github.com/pbberlin/tools/namespaces_taskqueues"
+	_ "github.com/pbberlin/tools/pbfetch"
+	_ "github.com/pbberlin/tools/pboauth"
+	_ "github.com/pbberlin/tools/write_methods"
 )
-
-var sq func(a ...interface{}) string = fmt.Sprint
-var spf func(format string, a ...interface{}) string = fmt.Sprintf
-var pf func(format string, a ...interface{}) (int, error) = fmt.Printf
-var opf func(w io.Writer, format string, a ...interface{}) (int, error) = fmt.Fprintf
 
 func init() {
 
-	http.HandleFunc("/login", login)
-	//http.HandleFunc("/", util_err.Adapter(big_query.ViewHTML))
 	log.Println("init() for mod01 (alias 'default') complete")
 	//util_err.StackTrace(5)
 
