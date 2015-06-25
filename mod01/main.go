af667c9bcd5d6bad8dddfb8215b8bee9e11fe669
Peter Buchmann <peter.buchmann@web.de>
2015-06-25 21:09:11 +0200

    adapt to package reorg

--- mod01/main.go
+++ mod01/main.go
@@ -7,26 +7,24 @@ import (
 
 	// not used - but init() functions wanted for
 	// httpHandler registrations
-	_ "github.com/pbberlin/tools/backend"
+	_ "github.com/pbberlin/tools/appengine/backend"
+	_ "github.com/pbberlin/tools/appengine/blobstore_mgt"
+	_ "github.com/pbberlin/tools/appengine/fulltext"
+	_ "github.com/pbberlin/tools/appengine/guestbook"
+	_ "github.com/pbberlin/tools/appengine/instance_mgt"
+	_ "github.com/pbberlin/tools/appengine/namespaces_taskqueues"
 	_ "github.com/pbberlin/tools/big_query"
-	_ "github.com/pbberlin/tools/blobstore_mgt"
 	_ "github.com/pbberlin/tools/dsu/ancestored_urls"
 	_ "github.com/pbberlin/tools/dsu/persistent_cursor"
 	_ "github.com/pbberlin/tools/email"
 	_ "github.com/pbberlin/tools/foscam"
-	_ "github.com/pbberlin/tools/fulltext"
-	_ "github.com/pbberlin/tools/guestbook"
-	_ "github.com/pbberlin/tools/instance_mgt"
 	_ "github.com/pbberlin/tools/json"
-	_ "github.com/pbberlin/tools/namespaces_taskqueues"
-	_ "github.com/pbberlin/tools/pbfetch"
+	_ "github.com/pbberlin/tools/net/http/proxy1"
 	_ "github.com/pbberlin/tools/pboauth"
 	_ "github.com/pbberlin/tools/write_methods"
 )
 
 func init() {
-
 	log.Println("init() for mod01 (alias 'default') complete")
 	//util_err.StackTrace(5)
-
 }
--- mod02/main.go
+++ mod02/main.go
@@ -6,41 +6,36 @@ import (
 	"appengine"
 
 	"fmt"
-	
+
 	"log"
 
-	// not used - but init() functions wanted for 
+	// not used - but init() functions wanted for
 	// httpHandler registrations
 	_ "github.com/pbberlin/tools/instance_mgt"
-
 )
-
 
 func init() {
 	// DISTINCT from other modules
-	
-	http.HandleFunc("/mod02" , mainMod02)
 
-	// _ah/start and _ah/stop  seem to be 
+	http.HandleFunc("/mod02", mainMod02)
+
+	// _ah/start and _ah/stop  seem to be
 	// the instance start and stop requests
 
 	log.Println("mod02 init complete")
 
 }
 
-
 func mainMod02(w http.ResponseWriter, r *http.Request) {
 
-	c := appengine.NewContext(r)	
+	c := appengine.NewContext(r)
 	module := appengine.ModuleName(c)
 
-	w.Header().Set("Content-Type", "text/html")	
+	w.Header().Set("Content-Type", "text/html")
 
-	instanceId := appengine.InstanceID()	
-	
-	w.Write( []byte(  fmt.Sprintf("Module -%v- <br>\n",module)   ) ) 
-	w.Write( []byte(  fmt.Sprintf("Instance -%v- <br>\n",instanceId)   ) ) 
+	instanceId := appengine.InstanceID()
 
+	w.Write([]byte(fmt.Sprintf("Module -%v- <br>\n", module)))
+	w.Write([]byte(fmt.Sprintf("Instance -%v- <br>\n", instanceId)))
 
 }
-
