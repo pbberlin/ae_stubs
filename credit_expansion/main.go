ca57aed3b7ac3bf642427a2fa8c6ec21e50dab55
Peter Buchmann <peter.buchmann@web.de>
2015-09-22 14:06:06 +0200

    credit-expansion serves from root now

--- credit_expansion/main.go
+++ credit_expansion/main.go
@@ -1,38 +1,94 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 
-	"github.com/pbberlin/tools/net/http/upload"
+	"appengine"
 
-	_ "net/http/pprof" // profiling  ... http://.../debug/pprof
+	"github.com/pbberlin/tools/net/http/fileserver"
+	"github.com/pbberlin/tools/net/http/htmlfrag"
+	"github.com/pbberlin/tools/net/http/loghttp"
+	"github.com/pbberlin/tools/net/http/tplx"
+	"github.com/pbberlin/tools/net/http/upload" // upload receive
+	"github.com/pbberlin/tools/os/fsi/dsfs"
+	"github.com/pbberlin/tools/os/fsi/memfs"
+	"github.com/pbberlin/tools/os/fsi/webapi"
+)
 
-	// not used - but init() functions wanted for
-	// httpHandler registrations
-	_ "github.com/pbberlin/tools/appengine/backend"
-	_ "github.com/pbberlin/tools/appengine/blobstore_mgt"
-	_ "github.com/pbberlin/tools/appengine/fulltext"
-	_ "github.com/pbberlin/tools/appengine/guestbook"
-	_ "github.com/pbberlin/tools/appengine/instance_info"
-	_ "github.com/pbberlin/tools/appengine/namespaced_taskqueued_cntr"
-	_ "github.com/pbberlin/tools/big_query"
-	_ "github.com/pbberlin/tools/dsu/ancestored_urls"
-	_ "github.com/pbberlin/tools/dsu/persistent_cursor"
-	_ "github.com/pbberlin/tools/email"
-	_ "github.com/pbberlin/tools/foscam"
-	_ "github.com/pbberlin/tools/net/http/proxy1"
-	_ "github.com/pbberlin/tools/oauthpb"
-	// _ "github.com/pbberlin/tools/os/fsi/dsfs"
-	_ "github.com/pbberlin/tools/util" // counter reset
-	_ "github.com/pbberlin/tools/write_methods"
+var fs1 = memfs.New(
+	memfs.Ident("mnt02"), // a closured variable in init() did not survive map-pointer reallocation
 )
 
 func init() {
-	http.HandleFunc("/backend1", backendHandler)
+	upload.InitHandlers()
+	http.HandleFunc(webapi.UriDeleteSubtree, loghttp.Adapter(webapi.DeleteSubtree))
+
+	http.HandleFunc("/backend-secret", backendHandler)
+
+	dynSrv := func(w http.ResponseWriter, r *http.Request, m map[string]interface{}) {
+		appID := appengine.AppID(appengine.NewContext(r))
+		if appID == "credit-expansion" {
+
+			prefix := "/mnt02"
+			// prefix = "/xxx"
+
+			fs2 := dsfs.New(
+				dsfs.MountName(prefix[1:]),
+				dsfs.AeContext(appengine.NewContext(r)),
+			)
+
+			fs1.SetOption(
+				memfs.ShadowFS(fs2),
+			)
+
+			//
+			// TRICK
+			// making FsiFileServer dream, that the mount prefix was
+			r.URL.Path = prefix + r.URL.Path
+			fileserver.FsiFileServer(fs1, prefix, w, r)
+		} else {
+			w.Write([]byte("app id is -" + appID + "- "))
+		}
+	}
+	http.HandleFunc("/", loghttp.Adapter(dynSrv))
+
+	//
+	dmpMemfs := func(w http.ResponseWriter, r *http.Request, m map[string]interface{}) {
+		htmlfrag.SetNocacheHeaders(w)
+		r.Header.Set("X-Custom-Header-Counter", "nocounter")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte("<pre>"))
+		w.Write(fs1.Dump())
+	}
+	http.HandleFunc("/memfsdmp", loghttp.Adapter(dmpMemfs))
+
 }
 
+var wpf = fmt.Fprint
+
 func backendHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("I am backend<br>\n<br>\n"))
-	uiUpload := upload.BackendUIRendered()
-	w.Write(uiUpload.Bytes())
+
+	lg, b := loghttp.BuffLoggerUniversal(w, r)
+	_ = lg
+	closureOverBuf := func(bUnused *bytes.Buffer) {
+		loghttp.Pf(w, r, b.String())
+	}
+	defer closureOverBuf(b) // the argument is ignored,
+
+	r.Header.Set("X-Custom-Header-Counter", "nocounter")
+
+	wpf(w, tplx.ExecTplHelper(tplx.Head, map[string]string{"HtmlTitle": "Static uploading and file serving"}))
+	defer wpf(w, tplx.Foot)
+
+	htmlfrag.Wb(w, "secret backend", "")
+	htmlfrag.Wb(w, "to root", "/", " ")
+
+	wpf(w, upload.BackendUIRendered().String())
+
+	htmlfrag.Wb(w, "fsi tools", "")
+	htmlfrag.Wb(w, "remove subtr", webapi.UriDeleteSubtree, " ")
+	htmlfrag.Wb(w, "memfs dump", "/memfsdmp", " ")
+
 }
