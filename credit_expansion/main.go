8f7a01cb0580af2550b274cd7c016f36bf5ff7db
Peter Buchmann <peter.buchmann@web.de>
2015-10-28 15:03:59 +0100

    tying together gitkit, payment, transition and forwarding

--- credit_expansion/main.go
+++ credit_expansion/main.go
@@ -10,6 +10,9 @@ import (
 
 	"appengine"
 
+	"github.com/pbberlin/tools/appengine/login"
+	"github.com/pbberlin/tools/appengine/login/gitkit1"
+	"github.com/pbberlin/tools/appengine/memcachepb"
 	"github.com/pbberlin/tools/dsu"
 	"github.com/pbberlin/tools/net/http/coinbase"
 	"github.com/pbberlin/tools/net/http/fileserver"
@@ -17,7 +20,6 @@ import (
 	"github.com/pbberlin/tools/net/http/loghttp"
 	"github.com/pbberlin/tools/net/http/tplx"
 	"github.com/pbberlin/tools/net/http/upload" // upload receive
-	"github.com/pbberlin/tools/oauthpb"
 	"github.com/pbberlin/tools/os/fsi/dsfs"
 	"github.com/pbberlin/tools/os/fsi/memfs"
 	"github.com/pbberlin/tools/os/fsi/webapi"
@@ -27,13 +29,22 @@ var fs1 = memfs.New(
 	memfs.Ident(tplx.TplPrefix[1:]), // a closured variable in init() did not survive map-pointer reallocation
 )
 
+var (
+	successLandingURL = "/auth/signin-landing"
+	signoutLandingURL = "/auth/signout-landing"
+)
+
 func init() {
 
 	upload.InitHandlers()
 	coinbase.InitHandlers()
+	tplx.InitHandlers()
+	login.InitHandlers()
+	gitkit1.InitHandlers()
+	memcachepb.InitHandlers()
 	http.HandleFunc(webapi.UriDeleteSubtree, loghttp.Adapter(webapi.DeleteSubtree))
 
-	http.HandleFunc("/backend-secret", backendHandler)
+	http.HandleFunc("/backend-reduced", backendHandler)
 
 	dynSrv := func(w http.ResponseWriter, r *http.Request, m map[string]interface{}) {
 
@@ -43,93 +54,116 @@ func init() {
 		r.Header.Set("X-Custom-Header-Counter", "nocounter")
 		htmlfrag.SetNocacheHeaders(w)
 
-		if !strings.Contains(r.URL.Path, "/member/") {
-			serveFromRoot(w, r)
+		err := r.ParseForm()
+		lg(err)
 
+		if !strings.Contains(r.URL.Path, "/member/") {
+			serveFromRoot(w, r, map[string][]byte{})
+			return
+		}
+
+		//
+		// ok, usr, msg := login.CheckForNormalUser(r)
+		// if msg != "" {
+		// 	msg += "<br>"
+		// }
+		// if !ok {
+		// 	w.Write([]byte(msg))
+		// 	return
+		// }
+
+		usr := gitkit1.CurrentUser(r)
+		if ok := gitkit1.IsSignedIn(r); !ok {
+			usr = nil
+		}
+		if usr == nil {
+			http.Redirect(w, r, gitkit1.WidgetSigninAuthorizedRedirectURL+"?mode=select&user=wasNil&red="+r.URL.Path, http.StatusFound)
+		}
+
+		//
+		//
+		usrID := "32168-unknown-user" // prevent method call on nil further down
+		if usr != nil {
+			usrID = usr.ID
+		}
+
+		someUserGreeting := new(bytes.Buffer)
+		wpf(someUserGreeting, tplx.ExecTplHelper(gitkit1.UserInfoHTML, map[string]interface{}{
+			"User": usr,
+		}))
+
+		invoice, err := dsu.BufGet(appengine.NewContext(r), "dsu.WrapBlob__"+usrID+r.URL.Path)
+		lg(err)
+
+		origTransactionMap := ""
+		if len(invoice.VByte) > 0 && r.Form.Get("transaction-details") != "" {
+			origTransactionMap = "<br>\n<pre>" + string(invoice.VByte) + "</pre>"
+		}
+
+		buyStatus := ""
+		if err != nil || invoice.I == 0 ||
+			invoice.Name == "" || invoice.Desc != "completed" {
+			buyStatus += "For this article we request a small contribution.<br>"
+			buyStatus += "You have not bought this article yet.<br>"
 		} else {
 
-			auth, usr, msg := oauthpb.Auth(r)
-			if msg != "" {
-				msg += "<br>"
-			}
+			if r.Form.Get("noredirect") == "" {
 
-			usrID := "32168-unknown-user" // prevent method call on nil further down
-			if usr != nil {
-				usrID = usr.ID
-			}
+				idc := "<br>" + gitkit1.GetIDCardTpl(w, r, usr)
 
-			if auth == false {
+				tm := time.Unix(int64(invoice.I), 0).Format("02 Jan 2006 at 15:04")
 
-				w.Write([]byte(msg))
-				return
+				Satoshi := int64(invoice.F * (1000 * 1000 * 100))
 
-			} else {
-
-				invoice, err := dsu.BufGet(appengine.NewContext(r), "dsu.WrapBlob__"+usrID+r.URL.Path)
-				lg(err)
-				buyStatus := ""
-				fullJSONData := ""
-				if err != nil {
-					buyStatus = "You have not bought this article yet.<br>"
-				} else {
-
-					tm := time.Unix(int64(invoice.I), 0)
-					buyStatus = fmt.Sprintf("status %v - UID %v Amount %v - at %v<br>",
-						invoice.Desc, invoice.Name, invoice.F, tm)
-					// fullJSONData = "<pre>" + string(invoice.VByte) + "</pre>"
-
-					if invoice.Desc == "completed" {
-						serveFromRoot(w, r)
-						return
-					}
-
-				}
-
-				//
-
-				btnTest := `
-					<div style='height:10px;'>&nbsp;</div>
-					<a class="coinbase-button" 
-						data-code="0025d69ea925b48ba2b7adeb2a911ca2" 
-						data-custom="productID=` + r.URL.Path + `&uID=` + usrID + `" 
-						data-env="sandbox" 
-						href="https://sandbox.coinbase.com/checkouts/0025d69ea925b48ba2b7adeb2a911ca2" 
-					>Pay With Bitcoin</a>
-					<script src="https://sandbox.coinbase.com/assets/button.js" type="text/javascript"></script>				
-				`
-
-				btnLive := `
-					<div style='height:10px;'>&nbsp;</div>
-					<a class="coinbase-button" 
-						data-code="aa4e03abbc5e2f5321d27df32756a932" 
-						data-custom="productID=` + r.URL.Path + `&uID=` + usrID + `" 
-						href="https://www.coinbase.com/checkouts/aa4e03abbc5e2f5321d27df32756a932" 
-					>Pay With Bitcoin</a>
-					<script src="https://www.coinbase.com/assets/button.js" type="text/javascript"></script>
-
-				`
-
-				_, _ = btnLive, btnTest
+				// buyStatus += fmt.Sprintf("You bought this article %v for %2.8f BTC.<br>", tm, invoice.F)
+				buyStatus += "<div style='font-size:80%;line-height:135%;margin-top:5px;'>"
+				buyStatus += fmt.Sprintf("You bought this article on %v for %v Satoshi.<br>", tm, Satoshi)
+				buyStatus += "Thanks for your business. Read your purchased article below."
+				buyStatus += "</div>"
+				buyStatus += "<hr>"
 
 				backPath := strings.Replace(r.URL.Path, "/member", "", 1)
-				backAnch := fmt.Sprintf("<a href='%v'>Back to introduction</a><br>", backPath)
+				backAnch := fmt.Sprintf("<br><a href='%v'>Back to introduction</a>.<br>", backPath)
 
-				bstpl := tplx.BootstrapTemplate(w, r)
-				wpf(w,
-					tplx.ExecTplHelper(bstpl, map[string]interface{}{
-						"HtmlTitle":       "Access restricted",
-						"HtmlDescription": "", // reminder
-						"HtmlContent": template.HTML("Access is restricted<br>" +
-							msg +
-							btnLive + "<br>" +
-							backAnch +
-							buyStatus +
-							fullJSONData +
-							"<br>")}))
-
+				serveFromRoot(w, r, map[string][]byte{
+					"</head>": []byte(gitkit1.Headers + "\n</head>"),
+					"<span id='REPLACE_BEFORE_CONTENT'></span>": []byte(idc + buyStatus + origTransactionMap),
+					"<span id='REPLACE_AFTER_CONTENT'></span>":  []byte(backAnch),
+					// "<span id='REPLACE_BEFORE_CONTENT'></span>": []byte(buyStatus),
+				})
 				return
 			}
+
 		}
+
+		btnHTML := fmt.Sprintf(coinbase.BtnLiveFormat, r.URL.Path, usrID)
+		if r.Form.Get("dbg") > "05" {
+			// coded hint, that we are in testing mode
+			btnHTML = fmt.Sprintf(coinbase.BtnTestFormat, r.URL.Path, usrID)
+			btnHTML += "<div style='display:inline-block;font-size:12px;margin-top:-20px;vertical-align:middle'>coinbase</div>"
+		}
+
+		backPath := strings.Replace(r.URL.Path, "/member", "/transition", 1)
+		backAnch := fmt.Sprintf("<a href='%v'>Back to introduction</a><br>", backPath)
+
+		bstpl := tplx.TemplateFromHugoPage(w, r)
+
+		wpf(w, tplx.ExecTplHelper(bstpl, map[string]interface{}{
+			"HtmlTitle":       "Access restricted",
+			"HtmlDescription": "", // reminder
+			"HtmlHeaders":     template.HTML(gitkit1.Headers),
+			"HtmlContent": template.HTML(
+				"<br>" +
+					gitkit1.GetIDCardTpl(w, r, usr) + "<br>" +
+					buyStatus +
+					btnHTML + "<br>" + "<br>" +
+					someUserGreeting.String() + "<br>" +
+					backAnch +
+					origTransactionMap +
+					"<br>"),
+		},
+		),
+		)
 
 	}
 	http.HandleFunc("/", loghttp.Adapter(dynSrv))
@@ -161,7 +195,7 @@ func init() {
 	http.HandleFunc("/reset-memfs", loghttp.Adapter(resetMemfs))
 }
 
-var wpf = fmt.Fprint
+var wpf = fmt.Fprintf
 
 func backendHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -174,7 +208,16 @@ func backendHandler(w http.ResponseWriter, r *http.Request) {
 
 	r.Header.Set("X-Custom-Header-Counter", "nocounter")
 
-	wpf(w, tplx.ExecTplHelper(tplx.Head, map[string]interface{}{"HtmlTitle": "Static uploading and file serving"}))
+	if ok, _, msg := login.CheckForAdminUser(r); !ok {
+		w.Write([]byte(msg))
+		return
+	}
+
+	wpf(w, tplx.ExecTplHelper(tplx.Head,
+		map[string]interface{}{
+			"HtmlTitle": "Static uploading and file serving",
+		}),
+	)
 	defer wpf(w, tplx.Foot)
 
 	htmlfrag.Wb(w, "secret backend", "")
@@ -188,10 +231,13 @@ func backendHandler(w http.ResponseWriter, r *http.Request) {
 	htmlfrag.Wb(w, "memfs reset", "/reset-memfs", " ")
 
 	wpf(w, coinbase.BackendUIRendered().String())
+	wpf(w, tplx.BackendUIRendered().String())
+	wpf(w, login.BackendUIRendered().String())
+	wpf(w, gitkit1.BackendUIRendered().String())
 
 }
 
-func serveFromRoot(w http.ResponseWriter, r *http.Request) {
+func serveFromRoot(w http.ResponseWriter, r *http.Request, replacements map[string][]byte) {
 
 	appID := appengine.AppID(appengine.NewContext(r))
 	if appID == AllowedAppID {
@@ -208,7 +254,7 @@ func serveFromRoot(w http.ResponseWriter, r *http.Request) {
 		// TRICK
 		// Making FsiFileServer dream, that the request path contained the mount prefix
 		r.URL.Path = tplx.TplPrefix + r.URL.Path
-		fileserver.FsiFileServer(fs1, tplx.TplPrefix, w, r)
+		fileserver.FsiFileServer(w, r, fileserver.Options{FS: fs1, Prefix: tplx.TplPrefix, Replacements: replacements})
 	} else {
 		w.Write([]byte("wrong app id: " + appID + "- "))
 	}
--- tec_news/main.go
+++ tec_news/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strings"
 	"time"
@@ -46,7 +45,7 @@ func init() {
 
 	http.HandleFunc("/backend-reduced", backendHandler)
 
-	dynSrv := func(w http.ResponseWriter, r *http.Request, m map[string]interface{}) {
+	staticOrMember := func(w http.ResponseWriter, r *http.Request, m map[string]interface{}) {
 
 		lg, b := loghttp.BuffLoggerUniversal(w, r)
 		_ = b
@@ -57,10 +56,21 @@ func init() {
 		err := r.ParseForm()
 		lg(err)
 
-		if !strings.Contains(r.URL.Path, "/member/") {
-			serveFromRoot(w, r, map[string][]byte{})
+		if !strings.Contains(r.URL.Path, "/member/") && !strings.Contains(r.URL.Path, "/transition/") {
+			serveFromRoot(w, r, map[string][]byte{}, false)
 			return
 		}
+
+		freePath := strings.Replace(r.URL.Path, "/member", "", 1)
+		freePath = strings.Replace(freePath, "/transition", "", 1)
+		freeAnch := fmt.Sprintf("<br><a href='%v'>Back to introduction</a>.<br>", freePath)
+
+		transPath := strings.Replace(r.URL.Path, "/member", "/transition", 1)
+		isTransition := strings.Contains(r.URL.Path, "/transition")
+		// transAnch := fmt.Sprintf("<a href='%v'>Back to introduction</a><br>", transPath)
+
+		memberPath := strings.Replace(r.URL.Path, "/transition", "/member", 1)
+		isMember := strings.Contains(r.URL.Path, "/member")
 
 		//
 		// ok, usr, msg := login.CheckForNormalUser(r)
@@ -77,7 +87,17 @@ func init() {
 			usr = nil
 		}
 		if usr == nil {
-			http.Redirect(w, r, gitkit1.WidgetSigninAuthorizedRedirectURL+"?mode=select&user=wasNil&red="+r.URL.Path, http.StatusFound)
+
+			if isMember {
+				http.Redirect(w, r, gitkit1.WidgetSigninAuthorizedRedirectURL+"?mode=select&user=wasNil&red="+r.URL.Path, http.StatusFound)
+				return
+
+			}
+			if isTransition {
+				// serveFromRoot(w, r, map[string][]byte{})
+				// return
+			}
+
 		}
 
 		//
@@ -92,49 +112,51 @@ func init() {
 			"User": usr,
 		}))
 
-		invoice, err := dsu.BufGet(appengine.NewContext(r), "dsu.WrapBlob__"+usrID+r.URL.Path)
+		invoice, err := dsu.BufGet(appengine.NewContext(r), "dsu.WrapBlob__"+usrID+memberPath)
 		lg(err)
 
 		origTransactionMap := ""
 		if len(invoice.VByte) > 0 && r.Form.Get("transaction-details") != "" {
-			origTransactionMap = "<br>\n<pre>" + string(invoice.VByte) + "</pre>"
+			origTransactionMap = "<br>\n<pre>" + string(invoice.VByte) + "</pre><br>"
 		}
 
 		buyStatus := ""
-		if err != nil || invoice.I == 0 ||
-			invoice.Name == "" || invoice.Desc != "completed" {
-			buyStatus += "For this article we request a small contribution.<br>"
-			buyStatus += "You have not bought this article yet.<br>"
-		} else {
+		idc := gitkit1.GetIDCardTpl(w, r, usr, transPath, transPath) // Signout to transition
+
+		// Bought
+		if invoice.I != 0 && invoice.Name != "" && invoice.Desc == "completed" {
 
 			if r.Form.Get("noredirect") == "" {
 
-				idc := "<br>" + gitkit1.GetIDCardTpl(w, r, usr)
+				if !isMember {
+					// Forward from transitive to member
+					http.Redirect(w, r, memberPath, http.StatusFound)
+					return
+				}
 
 				tm := time.Unix(int64(invoice.I), 0).Format("02 Jan 2006 at 15:04")
-
 				Satoshi := int64(invoice.F * (1000 * 1000 * 100))
 
 				// buyStatus += fmt.Sprintf("You bought this article %v for %2.8f BTC.<br>", tm, invoice.F)
-				buyStatus += "<div style='font-size:80%;line-height:135%;'>"
+				buyStatus += "<div style='font-size:80%;line-height:135%;margin-top:5px;'>"
 				buyStatus += fmt.Sprintf("You bought this article on %v for %v Satoshi.<br>", tm, Satoshi)
 				buyStatus += "Thanks for your business. Read your purchased article below."
 				buyStatus += "</div>"
 				buyStatus += "<hr>"
 
-				backPath := strings.Replace(r.URL.Path, "/member", "", 1)
-				backAnch := fmt.Sprintf("<br><a href='%v'>Back to introduction</a>.<br>", backPath)
-
 				serveFromRoot(w, r, map[string][]byte{
 					"</head>": []byte(gitkit1.Headers + "\n</head>"),
-					"<span id='REPLACE_BEFORE_CONTENT'></span>": []byte(idc + buyStatus + origTransactionMap),
-					"<span id='REPLACE_AFTER_CONTENT'></span>":  []byte(backAnch),
-					// "<span id='REPLACE_BEFORE_CONTENT'></span>": []byte(buyStatus),
-				})
+					"<span id='REPLACE_BEFORE_CONTENT'></span>": []byte("<br>" + idc + buyStatus + origTransactionMap),
+					"<span id='REPLACE_AFTER_CONTENT'></span>":  []byte(freeAnch),
+				}, false)
 				return
 			}
 
 		}
+
+		// Not bought yet
+		buyStatus += "For this article we request <i>52 Cents</i> compensation via Bitcoin.<br>"
+		// buyStatus += "You have not bought this article yet.<br>"
 
 		btnHTML := fmt.Sprintf(coinbase.BtnLiveFormat, r.URL.Path, usrID)
 		if r.Form.Get("dbg") > "05" {
@@ -143,30 +165,18 @@ func init() {
 			btnHTML += "<div style='display:inline-block;font-size:12px;margin-top:-20px;vertical-align:middle'>coinbase</div>"
 		}
 
-		backPath := strings.Replace(r.URL.Path, "/member", "/transition", 1)
-		backAnch := fmt.Sprintf("<a href='%v'>Back to introduction</a><br>", backPath)
-
-		bstpl := tplx.TemplateFromHugoPage(w, r)
-
-		wpf(w, tplx.ExecTplHelper(bstpl, map[string]interface{}{
-			"HtmlTitle":       "Access restricted",
-			"HtmlDescription": "", // reminder
-			"HtmlHeaders":     template.HTML(gitkit1.Headers),
-			"HtmlContent": template.HTML(
-				"<br>" +
-					gitkit1.GetIDCardTpl(w, r, usr) + "<br>" +
-					buyStatus +
-					btnHTML + "<br>" + "<br>" +
-					someUserGreeting.String() + "<br>" +
-					backAnch +
-					origTransactionMap +
-					"<br>"),
-		},
-		),
-		)
+		serveFromRoot(w, r, map[string][]byte{
+			"</head>": []byte(gitkit1.Headers + "\n</head>"),
+			"<span id='REPLACE_MID_CONTENT'></span>": []byte(idc + buyStatus +
+				btnHTML + "<br>" +
+				// someUserGreeting.String() + "<br>" +
+				origTransactionMap,
+			),
+			// "<span id='REPLACE_AFTER_CONTENT'></span>": []byte(freeAnch),
+		}, (usr != nil))
 
 	}
-	http.HandleFunc("/", loghttp.Adapter(dynSrv))
+	http.HandleFunc("/", loghttp.Adapter(staticOrMember))
 
 	//
 	dmpMemfs := func(w http.ResponseWriter, r *http.Request, m map[string]interface{}) {
@@ -237,7 +247,7 @@ func backendHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func serveFromRoot(w http.ResponseWriter, r *http.Request, replacements map[string][]byte) {
+func serveFromRoot(w http.ResponseWriter, r *http.Request, replacements map[string][]byte, cutout bool) {
 
 	appID := appengine.AppID(appengine.NewContext(r))
 	if appID == AllowedAppID {
@@ -254,7 +264,7 @@ func serveFromRoot(w http.ResponseWriter, r *http.Request, replacements map[stri
 		// TRICK
 		// Making FsiFileServer dream, that the request path contained the mount prefix
 		r.URL.Path = tplx.TplPrefix + r.URL.Path
-		fileserver.FsiFileServer(w, r, fileserver.Options{FS: fs1, Prefix: tplx.TplPrefix, Replacements: replacements})
+		fileserver.FsiFileServer(w, r, fileserver.Options{FS: fs1, Prefix: tplx.TplPrefix, Replacements: replacements, Cutout: cutout})
 	} else {
 		w.Write([]byte("wrong app id: " + appID + "- "))
 	}
