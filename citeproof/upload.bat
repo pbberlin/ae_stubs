9bde957dee71d399c8fe0355a29e0c5d77281709
pbberlin <peter.buchmann@web.de>
2017-04-21 17:19:38 +0200

    migrated to cloud sdk

--- citeproof/upload.bat
+++ citeproof/upload.bat
@@ -1,2 +1,3 @@
-appcfg.py --email=peter.buchmann@web.de update  mod01.yaml 
+REM python "c:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\appcfg.py" --email=peter.buchmann@web.de update  mod01.yaml 
+gcloud app deploy
 pause
